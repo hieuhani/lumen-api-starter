97647582d080f82af372eebd0a1d8d328000c074
Hieu Tran <hieutran.fu@gmail.com>
2018-11-12 16:00:47 +0700

    Token validation middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,7 +36,11 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            return response()->json([
+                'status' => 401,
+                'error' => 'Unauthorized',
+                'reason' => 'User is not registered'
+            ], 401);
         }
 
         return $next($request);
